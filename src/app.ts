8d51188b519e38559a387f82158db544c7f14f6c
João Maurício <joaomauricio1975@gmail.com>
2023-02-20 11:38:26 -0300

    conexão com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 class App {
 public app: express.Application;
@@ -7,11 +8,27 @@ public app: express.Application;
 public constructor() {
   this.app = express();
   this.app.use(cors());
+
+  this.initMongoose();
+  this.connectDatabase();
+}
+
+private initMongoose(): void {
+  mongoose.set('runValidators', true);
+}
+
+private connectDatabase(): void {
+  mongoose.connect('mongodb+srv://todoDashboard:zszJuAY9Nf7VWJQn@cluster0.84jm1nx.mongodb.net/curso-javascript?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
 }
 
 public listen(port: number): void {
   this.app.listen(port, () => {
-    console.log(`Aplicação iniciada na porta: ${port}`);
+    console.log(`Server running at port: ${port}`);
   });
 }
 }
